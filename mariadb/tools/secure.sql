d37c00fe4f69665f16a04c7ca2aa66c26022ab2b
Herinirina Livasoa <hrazafia@outlook.com>
2025-06-30 15:47:10 +0200

    update project

--- mariadb/tools/secure.sql
+++
@@ -1,6 +0,0 @@
-DROP USER IF EXISTS ''@'localhost', ''@'%';
-DROP USER IF EXISTS 'root'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db LIKE 'test\_%';
-FLUSH PRIVILEGES;
