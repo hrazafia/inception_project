73a020c1435e44c3606a2c1d693bbc5112c66111
Herinirina Livasoa <hrazafia@outlook.com>
2025-07-01 21:58:01 +0300

    update project

---
+++ nginx/tools/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -e
+
+CRT_FILE=/etc/ssl/certs/hrazafia.42.fr.crt
+KEY_FILE=/etc/ssl/private/hrazafia.42.fr.key
+
+if [ ! -f "$KEY_FILE" ] || [ ! -f "$CRT_FILE" ]; then
+  openssl req -newkey rsa:2048 -nodes -keyout "$KEY_FILE" \
+    -x509 -days 365 -out "$CRT_FILE" \
+    -subj "/C=MG/ST=Malagasy/L=Antananarivo/CN=hrazafia.42.fr"
+fi
+
+exec "$@"
