6f1e02dfab7970af2e6bf557d98e5fd2d09e0b0a
Herinirina Livasoa <hrazafia@outlook.com>
2025-07-01 23:02:52 +0300

    update project

--- nginx/tools/entrypoint.sh
+++ nginx/tools/entrypoint.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 set -e
 
-KEY_FILE=/etc/ssl/hrazafia.42.fr/hrazafia.42.fr.key
-CRT_FILE=/etc/ssl/hrazafia.42.fr/hrazafia.42.fr.crt
+CERT_DIR=/etc/ssl/hrazafia.42.fr
+KEY_FILE=${CERT_DIR}/hrazafia.42.fr.key
+CRT_FILE=${CERT_DIR}/hrazafia.42.fr.crt
 
-mkdir -p "/etc/ssl/hrazafia.42.fr"
+mkdir -p "$CERT_DIR"
 
 if [ ! -f "$KEY_FILE" ] || [ ! -f "$CRT_FILE" ]; then
   openssl req -newkey rsa:2048 -nodes -keyout "$KEY_FILE" \
-    -x509 -days 365 -out "$CRT_FILE" \
-    -subj "/C=MG/ST=Malagasy/L=Antananarivo/O=42/CN=hrazafia.42.fr"
+    -x509 -days 365 \
+    -out "$CRT_FILE" \
+    -subj "/C=FR/ST=Some-State/L=Some-City/O=MyOrg/CN=hrazafia.42.fr"
 fi
 
 exec "$@"
